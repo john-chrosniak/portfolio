1760301d70c1376df4adb199f25b8ce18eb147f9
john-chrosniak <chrosniakj@gmail.com>
2023-12-12 17:42:03 -0500

    started about

--- src/About.js
+++ src/About.js
@@ -5,7 +5,11 @@ import Col from 'react-bootstrap/Col';
 const About = () => {
     return (
         <Container className='fullScreen'>
-
+            <p>Hi, I'm John! I'm currently a Master's student at the University of Virginia conducting research in the area of 
+                autonomous racing. My research focuses on advancing the visual and physical intelligence of the AV-21, a full-scale autonomous racecar 
+                capable of travelling upwards of 180 mph. I'm extremely passionate about improving the safety and efficiency of autonomous mobile robots and
+                strive to use my proficient background in machine learning, dynamics modeling, and software engineering to solve complex problems.
+            </p>
         </Container>
     );
 }
