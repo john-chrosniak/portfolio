10de515c8ea17ec148f630c59545f2116c747b78
Brianna Heckert <briannaheckert@Briannas-MacBook-Pro.local>
2023-12-12 18:27:38 -0500

    footer spacing fix

--- src/Footer.js
+++ src/Footer.js
@@ -7,9 +7,9 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className="contactinfo">
-                <a style={{margin: "5px"}} href="https://www.linkedin.com/in/john-chrosniak/"><FontAwesomeIcon icon={faLinkedin} alt="LinkedIn Logo" style={{width:"50px", height:"50px", color:"#0077b5"}}/></a>
-                <a style={{margin: "5px"}} >jlc9wr@virginia.edu</a>
-                <a style={{margin: "5px"}} href="https://github.com/john-chrosniak"><FontAwesomeIcon icon={faGithub} alt="Github Logo" style={{width:"50px", height:"50px", color:"#000000"}}/></a>
+                <a style={{margin: "5px"}} href="https://www.linkedin.com/in/john-chrosniak/"><FontAwesomeIcon icon={faLinkedin} alt="LinkedIn Logo" style={{width:"40px", height:"40px", color:"#0077b5"}}/></a>
+                <a style={{marginTop: "13px"}} >jlc9wr@virginia.edu</a>
+                <a style={{margin: "5px"}} href="https://github.com/john-chrosniak"><FontAwesomeIcon icon={faGithub} alt="Github Logo" style={{width:"40px", height:"40px", color:"#000000"}}/></a>
             </div>
             <div className="copyright">
                 <p>&copy;John Chrosniak 2023</p>
