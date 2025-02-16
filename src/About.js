b9bc056d36ce4015bb59af5359758417d1294f5b
John Chrosniak <chrosniakj@gmail.com>
2025-02-16 12:43:08 -0500

    Updates (#1)
    * updated resume
    
    * Updated citation
    
    * Updated copyright
    
    * updated headshot
    
    * updated dependencies
    
    * updated about section

--- src/About.js
+++ src/About.js
@@ -3,7 +3,7 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Card } from 'react-bootstrap';
 
-import me_and_samir from './images/me_and_samir.jpg'
+import me_and_truck from './images/truck.jpg'
 import car_team from './images/car_team.png'
 import solar_car from './images/solar_car.jpg'
 import glacier from './images/glacier.jpg'
@@ -14,25 +14,29 @@ const About = () => {
             <Row>
                 <Col md={4}>
                     <Card className="text-center border-0">
-                        <Card.Img variant="top" src={me_and_samir} style={{height: "25rem"}}/>
+                        <Card.Img variant="top" src={me_and_truck} style={{height: "25rem"}}/>
                             {/* <Card.Body> */}
                                 {/* <Card.Title className="font-weight-bold">Me + Samir &lt;3</Card.Title> */}
                             {/* </Card.Body> */}
                     </Card>
                 </Col>
                 <Col md={8} className="my-auto">
-                <p className="lead" style={{textIndent: "50px"}}>Hi, I'm John! I'm currently a Master's student at the University of Virginia conducting research in the area of 
-                autonomous racing. My research focuses on advancing the visual and physical intelligence of the AV-21, a full-scale autonomous racecar 
-                capable of travelling upwards of 180 mph. I'm extremely passionate about improving the safety and efficiency of autonomous mobile robots and
-                strive to use my proficient background in machine learning, dynamics modeling, and software engineering to solve complex problems.
+                <p className="lead" style={{textIndent: "50px"}}>Hi, I'm John! I'm an embodied AI researcher passionate about investigating ways to improve the safety and 
+                agility of robotics and autonomous vehicles. I currently work full-time as a Connected Autonomous Vehicle Software Engineer for Leidos as part of the <a style={{color:"blue"}} 
+                href="https://highways.dot.gov/research/laboratories/saxton-transportation-operations-laboratory/saxton-transportation-operations-laboratory-overview">
+                Saxton Transportation Operations Laboratory</a>, where I focus on developing cooperative driving automation applications that improve the safety and efficiency of
+                autonomous vehicles. I also work part-time as a Race Engineer for Code19 Racing, a startup focused on competing in the <a style={{color:"blue"}} href="https://a2rl.io/">
+                Abu Dhabi Autonomous Racing Leaguue</a>, where I support the design and deployment of the motion planning and controls stack for a full-scale autonomous racecar that
+                races on the Yas Marina Formula One Circuit.
                 </p>
                 </Col>
             </Row>
             <Row className="pt-5">
                 <Col md={{span: 7, order: 1}} xs={{span:12, order: 2}} className="my-auto">
-                <p className="lead" style={{textIndent: "50px"}}>As a Perception Team Co-Lead on the <a style={{color:"blue"}} href="https://autonomousracing.dev/">Cavalier Autonomous Racing Team</a>,
-                I am in charge of developing and deploying the object detection and tracking software for a full-scale autonomous racecar. The University of Virginia team competes year round in time trial and head-to-head competitions
-                as part of the <a style={{color:"blue"}} href="https://www.indyautonomouschallenge.com/">Indy Autonomous Challenge </a>at professional racing tracks like the Indianapolis Motor Speedway, Las Vegas Motor Speedway, and the Monza Circuit.
+                <p className="lead" style={{textIndent: "50px"}}>As a graduate student at the University of Virginia, I led the Perception Sub-Team of the <a style={{color:"blue"}} href="https://autonomousracing.dev/">Cavalier Autonomous Racing Team</a>.
+                I was in charge of leading the design, development, and deployment of  the object detection and tracking software for a full-scale autonomous racecar that competes year round in time trial and head-to-head competitions
+                as part of the <a style={{color:"blue"}} href="https://www.indyautonomouschallenge.com/">Indy Autonomous Challenge </a>at professional racing tracks like the Indianapolis Motor Speedway, Las Vegas Motor Speedway, and Texas Motor Speedway.
+                My master's thesis focused on combining physics-based and data-driven approaches to model the complex dynamics of a racecar when operating at its handling limits.
                 </p>
                 </Col>
                 <Col xs={{ span: 12, order: 1 }} md={{ span: 5, order: 2 }}>
@@ -64,7 +68,7 @@ const About = () => {
             <Row className="pt-5">
                 <Col md={{span: 7, order: 1}} xs={{span:12, order: 2}} className="my-auto">
                 <p className="lead" style={{textIndent: "50px"}}>When I'm not hunched over a computer, I spend as much time as I can outdoors.
-                I love snowboarding, mountain biking, hiking, backpacking, and recently gave mountaineering a try (big fan of mountains). I also
+                I love running, snowboarding, mountain biking, hiking, backpacking, and recently gave mountaineering a try (big fan of mountains)! I also
                 enjoy cooking, video games, and playing the guitar, trombone, or trumpet.
                 </p>
                 </Col>
